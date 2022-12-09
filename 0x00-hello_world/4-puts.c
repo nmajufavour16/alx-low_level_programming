52998db3a692fb01ec8423090c792a9eb16d2bde
Nmaju Favour <nmajufavour16@gmail.com>
2022-12-09 15:40:53 -0800

    My second C lines of code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /*
 *main - print a line with puts command
 *return: always 0
 */
 
-int main(void);
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,9 +4,8 @@
 *return: always 0
 */
 
-int main(void);
+int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n")
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
-
