b9273cd159a3dfe7547577754f7e17737874569e
Nmaju Favour <nmajufavour16@gmail.com>
2022-12-09 15:25:15 -0800

    My second C lines of code

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+*main - Using the printf command to print a line
+*return: always 0
+*/
+
+int main(void);
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n")
+	return (0);
+}
+
