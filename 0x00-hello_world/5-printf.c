f9bc0817bf98b7ba4155a778cf0259f93f385a09
Nmaju Favour <nmajufavour16@gmail.com>
2022-12-09 16:17:17 -0800

    Solved betty issues
    cd ..

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /*
-*main - Using the printf command to print a line
-*return: always 0
-*/
+ * main - Using the printf command to print a line
+ *
+ * return: always 0 (success)
+ */
 
-int main(void);
+int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
